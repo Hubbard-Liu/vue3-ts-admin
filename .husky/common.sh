cd96f35251472c1e778c4a679eb94db3a1e05695
LiuYu <229645819@qq.com>
2022-02-08 11:47:11 +0800

    chore(deps): updated husky

--- .husky/common.sh
+++ .husky/common.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+###
+ # @Author: Do not edit
+ # @Date: 2022-02-08 11:40:06
+ # @LastEditors: Liuyu
+ # @LastEditTime: 2022-02-08 11:40:06
+ # @FilePath: \vue3-ts-init\.husky\common.sh
+###
 command_exists () {
   command -v "$1" >/dev/null 2>&1
 }
